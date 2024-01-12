31de13ffe3b7a7284e0e569030737f1d91f24fc4
fukazawa-lab <147732705+fukazawa-lab@users.noreply.github.com>
2024-01-12 23:31:45 +0900

    Update generate_training_data.py

--- generate_training_data.py
+++ generate_training_data.py
@@ -3,7 +3,7 @@ import argparse
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.utils import to_categorical
 import numpy as np
-
+from PIL import Image
 def load_data(data_parent_dir, class_names):
     # クラス数
     num_classes = len(class_names)
@@ -19,6 +19,24 @@ def load_data(data_parent_dir, class_names):
             print("■■■クラス名にスペースが入っています！スペースを削除してクラス名を再度定義しなおしてください。Colabを再起動（右上の▼ボタンの「ランタイムを接続解除して削除」）して再度画像を入れて実行してください。")
             return
             
+        source_folder = "cnn-class/image_resource/"+class_name+"/"  # 犬の画像がまとまっているフォルダ
+        # source_folder 内のすべての jpg ファイルを取得
+        image_files = [f for f in os.listdir(source_folder) if f.lower().endswith(".jpg")]
+
+        # 画像の読み込みとチェック
+        for image_file in image_files:
+            image_path = os.path.join(source_folder, image_file)
+            try:
+                img = Image.open(image_path)
+                img.verify()  # 画像が正常に読み込まれるかどうかのチェック
+                img.close()
+            except Exception as e:
+                print(f"■■■この画像ファイルは読み込めません！！削除してから一番初めから実行してください． {image_path}")
+                print(f"■■■ファイル名は{image_path}")
+                continue  # エラーが発生した場合はスキップ
+
+
+
     for class_idx, class_name in enumerate(class_names):
         # トレーニングデータの読み込み
         class_folder = os.path.join(data_parent_dir, class_name, 'output/train')
